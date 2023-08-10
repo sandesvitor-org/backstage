0b630c6a84e068f4ed715741908c2a9bc7b005f2
Vitor Sandes <sandesvitor@gmail.com>
2023-08-10 02:18:34 -0300

    added new config

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -1,4 +1,5 @@
 import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
+import { GithubOrgEntityProvider, GithubOrgReaderProcessor } from '@backstage/plugin-catalog-backend-module-github';
 import { ScaffolderEntitiesProcessor } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
@@ -7,7 +8,21 @@ export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
   const builder = await CatalogBuilder.create(env);
-  builder.addProcessor(new ScaffolderEntitiesProcessor());
+  builder.addEntityProvider(
+    GithubOrgEntityProvider.fromConfig(env.config, {
+      id: 'development',
+      orgUrl: 'https://github.com/sandesvitor-org',
+      logger: env.logger,
+      schedule: env.scheduler.createScheduledTaskRunner({
+        frequency: { minutes: 60 },
+        timeout: { minutes: 15 },
+      }),
+    }),
+  );
+  builder.addProcessor(
+    GithubOrgReaderProcessor.fromConfig(env.config, { logger: env.logger }),
+    new ScaffolderEntitiesProcessor(),
+  );
   const { processingEngine, router } = await builder.build();
   await processingEngine.start();
   return router;
